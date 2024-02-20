41eedf654a5d894e10734ea7157250f6000c0d82
Truong Hong Loc <hongloc2206@gmail.com>
2024-02-20 19:36:53 +0700

    add more colors

--- app/board/[boardId]/_components/color-picker.tsx
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -14,14 +14,22 @@ export const ColorPicker = ({
     <div
       className="flex flex-wrap gap-2 items-center max-w-[164px] pr-2 mr-2 border-r border-neutral-200"
     >
-      <ColorButton
+      <ColorButton color={{ r: 243, g: 82, b: 35 }} onClick={onChange} />
+      <ColorButton color={{ r: 255, g: 249, b: 177 }} onClick={onChange} />
+      <ColorButton color={{ r: 68, g: 202, b: 99 }} onClick={onChange} />
+      <ColorButton color={{ r: 39, g: 142, b: 237 }} onClick={onChange} />
+      <ColorButton color={{ r: 155, g: 105, b: 245 }} onClick={onChange} />
+      <ColorButton color={{ r: 252, g: 142, b: 42 }} onClick={onChange} />
+      <ColorButton color={{ r: 0, g: 0, b: 0 }} onClick={onChange} />
+      <ColorButton color={{ r: 255, g: 255, b: 255 }} onClick={onChange} />
+      {/* <ColorButton
         color={{
           r: 243,
           g: 82,
           b: 35
         }}
         onClick={onChange}
-      />
+      /> */}
       {/* <ColorButton />
       <ColorButton /> */}
     </div>
